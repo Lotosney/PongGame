741af2131f237f59c16e8eb09a65ce01d4f6ec57
Lotosney <sebastiandwojcik@gmail.com>
2021-05-06 18:50:40 +0200

    Add player paddle

--- game.js
+++ game.js
@@ -5,6 +5,15 @@ const h = canvas.height = 600
 document.body.appendChild(canvas)
 const context = canvas.getContext('2d')
 
+const paddleWidth = h*.12
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+        player.y -= 4
+    } else if (event.key === 'ArrowDown') {
+        player.y += 4
+    }
+})
 
 const board = () => {
     context.beginPath()
@@ -13,11 +22,22 @@ const board = () => {
     context.lineTo(w/2,h)
     context.stroke()
 }
+class Player {
+    constructor(y) {
+        this.y = y
+    }
+
+    draw = () => {
+        context.beginPath()  
+        context.fillRect(w*.03,this.y-(h*.06),w*.01, paddleWidth)
+    }
+}
+const player = new Player(h/2)
 
 
 function animate() {
 
-
+    player.draw()
     board()
     requestAnimationFrame(animate)
 }
